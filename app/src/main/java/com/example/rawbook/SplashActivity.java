27d08dc6803db43a51dbbd410fed5a64e4f9d939
SHUBHAM DHINGRA <shubhamdhingra.codebrew@gmail.com>
2019-12-01 22:51:51 +0530

    first commit -m 1st dec

--- app/src/main/java/com/example/rawbook/SplashActivity.java
+++ app/src/main/java/com/example/rawbook/SplashActivity.java
@@ -5,8 +5,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class Splash extends Activity
-    {
+public class SplashActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
     // TODO Auto-generated method stub
@@ -14,18 +14,24 @@ public class Splash extends Activity
     setContentView(R.layout.splash);
 
     Thread timerThread = new Thread(){
+
     public void run(){
+        
     try{
-    sleep(2000);
-    }catch(InterruptedException e){
-    e.printStackTrace();
-    }finally{
-    Intent intent = new Intent(Splash.this,MainActivity.class);
-    startActivity(intent);
+         sleep(2000);
+    }
+    
+    catch(InterruptedException e){
+         e.printStackTrace();
+    }
+    
+    finally{
+             Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+             startActivity(intent);
     }
     }
     };
-    timerThread.start();
+         timerThread.start();
     }
 
     @Override
@@ -35,10 +41,4 @@ public class Splash extends Activity
     finish();
     }
 
-
-
-
-
-
-
-    }
+}
--- app/src/main/java/com/example/rawbook/login.java
+++
@@ -1,5 +0,0 @@
-package com.example.rawbook;
-
-public class login
-    {
-    }
