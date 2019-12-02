913665fef1a2436f7e0475cad554f485f91f6192
SHUBHAM DHINGRA <shubhamdhingra.codebrew@gmail.com>
2019-12-03 00:26:41 +0530

    update december 3

--- app/src/main/java/com/example/rawbook/LoginActivity.java
+++ app/src/main/java/com/example/rawbook/LoginActivity.java
@@ -1,14 +1,28 @@
 package com.example.rawbook;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
+
+    Button btnSignUp;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        btnSignUp =  (Button) findViewById(R.id.btnSignUp);
+        btnSignUp.setText(getString(R.string.underlined_dynamic_text_and_bold, "SignUp"));
+    }
+
+    public void signUpAct(View v){
+        Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
+        startActivity(intent);
     }
 }
 
